9a3c767bb5c183106b79339150d8fc897c8b55ac
gabriel2santosneto <gabriel2santosneto@gmail.com>
2025-11-08 12:51:56 -0500

    summer camp icon

--- src/$desktop.js
+++ src/$desktop.js
@@ -148,3 +148,44 @@ $(window).on("scroll focusin", () => {
 })();
 // === /Custom Desktop Icon ===
 
+
+// === Custom Desktop Icon: Summer Camp (added by ChatGPT) ===
+(function () {
+  try {
+    const $icon = $(`
+      <div class="desktop-icon" style="position:absolute; left:140px; top:40px; width:90px; text-align:center; cursor:default;">
+        <img src="images/icons/folder-pictures.png" width="32" height="32" alt="Summer Camp">
+        <div class="label" style="margin-top:4px; color:#000; text-shadow:1px 1px #fff; font-family:'MS Sans Serif', Tahoma, Arial; font-size:12px;">
+          Summer Camp
+        </div>
+      </div>
+    `);
+
+    function openSummerCamp() {
+      if (typeof window.openWindow === "function") {
+        openWindow({
+          title: "Summer Camp",
+          width: 720,
+          height: 520,
+          html: `<iframe src="summer camp/index.html" style="border:0; width:100%; height:100%;"></iframe>`
+        });
+      } else if (typeof window.openIframeApp === "function") {
+        openIframeApp({ title: "Summer Camp", src: "summer camp/index.html", width: 720, height: 520 });
+      } else {
+        window.open("summer camp/index.html", "_blank");
+      }
+    }
+
+    $icon.on("dblclick", openSummerCamp);
+    const $desktop = $(".desktop");
+    if ($desktop && $desktop.length) {
+      $desktop.append($icon);
+    } else {
+      setTimeout(() => $(".desktop").append($icon), 500);
+    }
+  } catch (e) {
+    console && console.error("Failed to add Summer Camp icon:", e);
+  }
+})();
+// === /Custom Desktop Icon: Summer Camp ===
+
